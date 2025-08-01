7ff8e49eaf1cb06dc259380e8125aac0a06ce85b
fatah <fathlbkart@gmail.com>
2025-08-01 15:32:31 +0100

    initial commit

---
+++ app/(landing-page)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold mb-4 font-stretch-ultra-expanded">
+        LANDING PAGE
+      </h1>
+    </div>
+  );
+}
