368269c969e0fcdb1f87c122b9895e13da7beee9
fatah <fathlbkart@gmail.com>
2025-08-01 16:30:47 +0100

    fix styles off

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,19 @@
+import {
+  Card,
+  CardTitle,
+  CardHeader,
+  CardDescription,
+} from "@/components/ui/card";
+
+const LoginPage = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Welcome Back !</CardTitle>
+        <CardDescription>Login using your Gmail account</CardDescription>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default LoginPage;
---
+++ tailwind.config.ts
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./app/**/*.{ts,tsx,js,jsx}", "./components/**/*.{ts,tsx,js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+      },
+    },
+  },
+  plugins: [],
+};
