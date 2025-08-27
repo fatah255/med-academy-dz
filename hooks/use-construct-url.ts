5c4b66a2efa8b593817f83e487e9f891e3820951
fatah <fathlbkart@gmail.com>
2025-08-27 23:58:11 +0100

    type fix

--- hooks/use-construct-url.ts
+++ hooks/use-construct-url.ts
@@ -1,5 +1,5 @@
 import { env } from "@/lib/env";
 
-export default function useConstructUrl(key: string) {
+export default function useConstructUrl(key: string | undefined | null) {
   return `https://${process.env.NEXT_PUBLIC_S3_BUCKET_NAME_IMAGES}.t3.storageapi.dev/${key}`;
 }
